fed0c1a7ba4a3f7023930b7442ba9f2b7534e91b
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-08-26 17:37:31 +0530

    added UART1 driver

--- drivers/uart/uart.c
+++ drivers/uart/uart.c
@@ -1,5 +1,53 @@
 #include "drivers/uart/uart.h"
+#include "mm/mm.h"
 
 void uart_init(){
+	
+	mm_w(AUX_ENABLES, mm_r(AUX_ENABLES) | 0x1);
+	mm_w(AUX_MU_IER_REG, 0);
+	mm_w(AUX_MU_CNTL_REG,0);
+	mm_w(AUX_MU_LCR_REG, 3); // uart 8-bit mode
+	mm_w(AUX_MU_MCR_REG, 0);
+	mm_w(AUX_MU_IER_REG, 0);
+	mm_w(AUX_MU_IIR_REG, 0xc6);
+	mm_w(AUX_MU_BAUD_REG, 270); // 115200 baudrate
+	mm_w(GPFSEL1, mm_r(GPFSEL1)& ~((0b11 << 12 | 0b11 << 15))); // reset GPIO Pins 14 and 15
+	mm_w(GPFSEL1, mm_r(GPFSEL1) | (0b10 << 14 | 0b10 << 15));
 
+	mm_w(GPPUD, 0);
+	for(int i=0;i<150;i++); // wait for 150 cycles
+	mm_w(GPPUDCLK0, ((1u << 14) | (1u << 15)));
+	for(int i=0;i<150;i++); // wait for 150 cycles
+	mm_w(GPPUDCLK0, 0);
+
+	mm_w(AUX_MU_CNTL_REG, 3);  //enable tx/rx
+
+}
+
+void uart_tx(char ch){
+	do{
+		asm volatile("nop");
+	}while(!(mm_r(AUX_MU_LSR_REG) & 0x20)); // wait for UART tx
+
+	mm_w(AUX_MU_IO_REG, ch);
+}
+
+char uart_rx(){
+	do{
+		asm volatile("nop");
+	}while(!(mm_r(AUX_MU_LSR_REG) & 0x01)); // wait for something to be written into buf.
+	
+	if(mm_r(AUX_MU_IO_REG) == '\r') return '\n'; // conv carriage ret into newline
+
+	return mm_r(AUX_MU_IO_REG);
+}
+
+void uart_write(char* str){
+	char* ptr = str;
+	while(*ptr){
+		if(*ptr == '\n') *ptr = '\r';
+
+		uart_tx(*ptr);
+		ptr++;
+	}
 }
--- include/mm/mm.h
+++ include/mm/mm.h
@@ -3,6 +3,51 @@
 
 #include <stdint.h>
 
+#define PERIPH_BASE		0x3f000000
+#define AUX_BASE		PERIPH_BASE + 0x215000
+#define GPIO_BASE		PERIPH_BASE + 0x200000
+
+#define AUX_IRQ 		(AUX_BASE + 0x0)
+#define AUX_ENABLES 		(AUX_BASE + 0x4)
+#define AUX_MU_IO_REG 		(AUX_BASE + 0x40)
+#define AUX_MU_IER_REG 		(AUX_BASE + 0x44)
+#define AUX_MU_IIR_REG 		(AUX_BASE + 0x48)
+#define AUX_MU_LCR_REG 		(AUX_BASE + 0x4c)
+#define AUX_MU_MCR_REG 		(AUX_BASE + 0x50)
+#define AUX_MU_LSR_REG 		(AUX_BASE + 0x54)
+#define AUX_MU_MSR_REG 		(AUX_BASE + 0x58)
+#define AUX_MU_SCRATCH 		(AUX_BASE + 0x5c)
+#define AUX_MU_CNTL_REG 	(AUX_BASE + 0x60)
+#define AUX_MU_STAT_REG 	(AUX_BASE + 0x64)
+#define AUX_MU_BAUD_REG 	(AUX_BASE + 0x68)
+#define AUX_SPI0_CNTL0_REG 	(AUX_BASE + 0x80)
+#define AUX_SPI0_CNTL1_REG 	(AUX_BASE + 0x84)
+#define AUX_SPI0_STAT_REG 	(AUX_BASE + 0x88)
+#define AUX_SPI0_IO_REG 	(AUX_BASE + 0x90)
+#define AUX_SPI0_PEEK_REG 	(AUX_BASE + 0x94)
+#define AUX_SPI1_CNTL0_REG 	(AUX_BASE + 0xc0)
+#define AUX_SPI1_CNTL1_REG 	(AUX_BASE + 0xc4)
+
+
+#define GPFSEL0         	(GPIO_BASE+0x00)
+#define GPFSEL1         	(GPIO_BASE+0x04)
+#define GPFSEL2         	(GPIO_BASE+0x08)
+#define GPFSEL3         	(GPIO_BASE+0x0C)
+#define GPFSEL4         	(GPIO_BASE+0x10)
+#define GPFSEL5         	(GPIO_BASE+0x14)
+#define GPSET0          	(GPIO_BASE+0x1C)
+#define GPSET1          	(GPIO_BASE+0x20)
+#define GPCLR0          	(GPIO_BASE+0x28)
+#define GPLEV0         		(GPIO_BASE+0x34)
+#define GPLEV1          	(GPIO_BASE+0x38)
+#define GPEDS0          	(GPIO_BASE+0x40)
+#define GPEDS1          	(GPIO_BASE+0x44)
+#define GPHEN0          	(GPIO_BASE+0x64)
+#define GPHEN1          	(GPIO_BASE+0x68)
+#define GPPUD           	(GPIO_BASE+0x94)
+#define GPPUDCLK0       	(GPIO_BASE+0x98)
+#define GPPUDCLK1       	(GPIO_BASE+0x9C)
+
 void 	 mm_w(uint64_t reg, uint64_t val);
 uint64_t mm_r(uint64_t reg);
 
