ec7af1063fdeded7beceaf25d809ef1b2e5396be
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-08-28 19:16:37 +0530

    small modification. ignore

--- core/main.c
+++ core/main.c
@@ -4,15 +4,8 @@
 
 void delta_main(void){
 	
-	uart_init(); //log("UART initialised");
-	if(sd_init() == SD_OK) log("SD initialised");
-	else{
-		panic("SD initialisation failed!");
-	}
-	unsigned char buf[100];
-	sd_readblock(0,buf, 1);
-
-	while(1) log((char*)buf);
+	uart_init();
+	sd_init();
 //	while(1) log("hello");
 
 	while(1);
