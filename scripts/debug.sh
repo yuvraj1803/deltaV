3eeb3e2aa13763eba328cd752ce4a0806e147870
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-08-29 13:56:18 +0530

    removed scripts

--- scripts/debug.sh
+++
@@ -1,3 +0,0 @@
-make 
-
-qemu-system-aarch64 -M raspi3b -nographic -serial null -serial mon:stdio -m 1024 -kernel ./kernel8.img -s -S
--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-make 
-#make SD_with_guests
-
-qemu-system-aarch64 -M raspi3b -nographic -serial null -serial mon:stdio -m 1024 -kernel ./kernel8.img -drive file=./SD_with_guests.img,if=sd,format=raw
