35b15964bbbb69b140bbbe213b849dcc62ad3d79
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-08-30 08:37:32 +0530

    minor log message format change

--- lib/src/stdio.c
+++ lib/src/stdio.c
@@ -2,13 +2,13 @@
 #include "drivers/uart.h"
 
 void log(char *str){
-	uart_write("[LOG]: ");
+	uart_write("LOG: ");
 	uart_write(str);
 	uart_write("\n");
 }
 
 void panic(char *str){
-	uart_write("[PANIC]: ");
+	uart_write("PANIC: ");
 	uart_write(str);
 	uart_write("\n");
 	while(1);
