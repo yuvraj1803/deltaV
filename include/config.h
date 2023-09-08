02e3311ed306b717d907d02fb2abe639e3168a35
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-09-08 11:41:13 +0530

    fixed SET() macro multiple args error

--- include/config.h
+++ include/config.h
@@ -1,19 +1,18 @@
 #ifndef __config_h__
 #define __config_h__
 
-#define SET(value, bit)		(value << bit)
 
 // EL3
-#define EL3_SCR 		SET(1,10) | SET(1,8) | SET(1,0)
+#define EL3_SCR 		(1U << 10)  | (1U << 8) | (1 << 0)
 //				    RW		HCE	    NS
-#define EL3_SPSR		SET(7,6) | SET(9,0)
+#define EL3_SPSR		(7 << 6) |  (9 << 0)
 //			         I+F MASK    EL2h
 
 
-// EL2
+// EL2K
 #define EL2_STACK_BASE		0x80000 - 0x1
-#define EL2_HCR			SET(1,21) | SET(1,18) | SET(1,17) | SET(1,16) | SET(1,14) | SET(1,13) | SET(0,34 | SET(1,31) | SET(0,27) | SET(1,5) | SET(1,4) | SET(1,3) | SET(1,1) | SET(1,0)
-//				TACR	    TID3       	TID2        TID1        TWE         TWI         E2H        RW          TGE         AMO        IMO        FMO        SWIO       VM
+#define EL2_HCR			 (1 << 21) |  (1 << 18) |  (1 << 17) |  (1 << 16) |  (1 << 14) |  (1 << 13) |  (0 << 34) |  (1 << 31) |  (0 << 27) |  (1 << 5) |  (1 << 4) |  (1 << 3) |  (1 << 1) |  (1 << 0)
+//				  TACR	        TID3   	      TID2        TID1        TWE           TWI            E2H         RW           TGE          AMO         IMO         FMO        SWIO       VM
 #define EL2_SPSR		0
 #define EL2_VTCR
 #define EL2_SCTLR
