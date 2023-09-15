7b11f6572c09399ffb2d63865056cee4e99bd9bd
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-09-15 15:17:04 +0530

    added el2 exception table, handler macros (still incomplete)

---
+++ include/core/exceptions.h
@@ -0,0 +1,28 @@
+#ifndef __IRQ_H__
+#define __IRQ_H__ 
+
+#define  CURRENT_EL_SP0_SYNC    0x0
+#define  CURRENT_EL_SP0_IRQ     0x1
+#define  CURRENT_EL_SP0_FIQ     0x2
+#define  CURRENT_EL_SP0_SERROR  0x3
+
+#define  CURRENT_EL_SPX_SYNC    0x4
+#define  CURRENT_EL_SPX_IRQ     0x5
+#define  CURRENT_EL_SPX_FIQ     0x6
+#define  CURRENT_EL_SPX_SERROR  0x7
+
+#define  LOWER_EL_A64_SYNC      0x8
+#define  LOWER_EL_A64_IRQ       0x9
+#define  LOWER_EL_A64_FIQ       0xa
+#define  LOWER_EL_A64_SERROR    0xb
+
+#define  LOWER_EL_A32_SYNC      0xc
+#define  LOWER_EL_A32_IRQ       0xd
+#define  LOWER_EL_A32_FIQ       0xe
+#define  LOWER_EL_A32_SERROR    0xf
+
+
+
+
+
+#endif
