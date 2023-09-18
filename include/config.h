713f64446a4a60a27c637f7224eab6428c5eb4e5
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-09-18 18:04:08 +0530

    cleaned up config file

--- include/config.h
+++ include/config.h
@@ -3,28 +3,57 @@
 
 
 // EL3
-#define EL3_SCR 		(1U << 10)  | (1U << 8) | (1 << 0)
-//				    RW		HCE	    NS
-#define EL3_SPSR		(7 << 6) |  (9 << 0)
-//			         I+F MASK    EL2h
+#define EL3_SCR 	/*RW*/	       (1U << 10)  |\
+			/*HCE*/        (1U << 8)   |\
+			/*NS*/         (1U << 0)
+			
+#define EL3_SPSR	/*I+F MASK*/	(7 << 6)   |\
+			/*EL2h*/	(9 << 0)
+
 
 
 // EL2
 #define EL2_STACK_BASE		0x80000 - 0x1
-#define EL2_HCR			 (1 << 21) |  (1 << 18) |  (1 << 17) |  (1 << 16) |  (1 << 14) |  (1 << 13) |  (0 << 34) |  (1 << 31) |  (0 << 27) |  (1 << 5) |  (1 << 4) |  (1 << 3) |  (1 << 1) |  (1 << 0)
-//				  TACR	        TID3   	      TID2        TID1        TWE           TWI            E2H         RW           TGE          AMO         IMO         FMO        SWIO       VM
-#define EL2_VTCR		(1 << 30) | (1 << 29) | (0 << 19) | (2 << 16) | (0 << 14) | (3 << 12) | (0 << 10) | (0 << 8) | (1 << 6) | (64 - 38)
-//				   NSA         NSW        VS          PS           TG0        SH0         ORGN0        IRGN0     SL0        T0SZ
-#define EL2_SCTLR_NO_MMU	(0 << 25) | (0 < 12) | (0 << 2) | (0 << 0)
-// 				  EE      I-CACHE->0  D-CACHE->0  MMU-OFF
+
+#define EL2_HCR	    /*TACR*/	 (1 << 21) |  \
+		    /*TID3*/	 (1 << 18) |  \
+		    /*TID2*/     (1 << 17) |  \
+	            /*TID1*/	 (1 << 16) |  \
+		    /*TWE*/	 (1 << 14) |  \
+		    /*TWI*/	 (1 << 13) |  \
+		    /*E2H*/	 (0 << 34) |  \
+		    /*RW*/	 (1 << 31) |  \
+      		    /*TGE*/	 (0 << 27) |  \
+		    /*AMO*/	 (1 << 5) |   \
+		    /*IMO*/	 (1 << 4) |   \
+		    /*FMO*/	 (1 << 3) |   \
+		    /*SWIO*/	 (1 << 1) |   \
+		    /*VM*/	 (1 << 0)
+		 
+
+#define EL2_VTCR	/*NSA*/		(1 << 30) | \
+			/*NSW*/		(1 << 29) | \
+			/*VS*/		(0 << 19) | \
+			/*PS*/		(2 << 16) | \
+			/*TG0*/		(0 << 14) | \
+			/*SH0*/		(3 << 12) | \
+			/*ORGN0*/	(0 << 10) | \
+			/*IRGN0*/	(0 << 8)  | \
+			/*SL0*/		(1 << 6)  | \
+			/*T0SZ*/	(64 - 38)
+
+
+#define EL2_SCTLR_NO_MMU	/*EE*/		(0 << 25) | \
+				/*I-CACHE*/	(0 < 12)  | \
+				/*D-CACHE*/	(0 << 2)  | \
+				/*MMU*/		(0 << 0)
 #define EL2_SCTLR_WITH_MMU	EL2_SCTLR_NO_MMU | (1 << 0)
+
 #define EL2_MAIR
+
 #define EL2_SPSR		EL3_SPSR
-#define EL2_TCR
+#define EL2_TCR	   /*T0SZ*/	(64-38) | \
+		   /*4K TG0*/	(0 << 14)
 
-
-#define FS_TOTAL_FILE_DESCRIPTORS 1024
-#define FS_MAX_PATH_UNIT_LENGTH	  108
-#define FS_MAX_PATH_LENGTH        1024
 
 #endif
