6ea84ead1d470bc0a41f31d350fcf1e2f2416438
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-09-24 19:48:55 +0530

    paging setup routines added

---
+++ include/mm/paging.h
@@ -0,0 +1,21 @@
+#ifndef __PAGING_H__
+#define __PAGING_H__
+
+#include <stdint.h>
+
+
+void mmu_init();
+
+void load_ttbr0_el2(uint64_t __page_dir_start);
+void load_tcr_el2();
+void load_vtcr_el2();
+void load_mair_el2();
+void clear_el1_tlb();
+void load_sctlr_with_mmu();
+void prepare_page_tables_and_map_memory(uint64_t __page_dir_start);
+
+struct vaddr_space{
+    uint64_t* lv1_table;
+};
+
+#endif
