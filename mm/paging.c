f955aa8ea4f70dc3bf25d373d0aacecb044ec8ba
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2023-09-24 19:48:46 +0530

    paging setup routines in paging.S called from mmu_init()

---
+++ mm/paging.c
@@ -0,0 +1,14 @@
+#include "mm/paging.h"
+
+extern volatile uint32_t __page_dir_start;
+
+void mmu_init(){
+
+	prepare_page_tables_and_map_memory(&__page_dir_start);
+	load_ttbr0_el2(&__page_dir_start);
+	load_tcr_el2();
+	load_vtcr_el2();
+	load_mair_el2();
+	clear_el1_tlb();
+	load_sctlr_with_mmu();
+}
