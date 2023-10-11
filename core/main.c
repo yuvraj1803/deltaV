f3e223e9c55ecdc3bc756b106d44fbfc45d73cb3
yuvraj1803 <ysakshith@gmail.com>
2023-10-11 15:20:01 +0530

    added sync.c and handle_sync() skeleton

--- core/main.c
+++ core/main.c
@@ -27,7 +27,7 @@ void main(void){
 	sched_init();
 	shell_init();
 
-	vm_init("/guests/hw.bin", 0x80000,0x80000,0x80000);
+	vm_init("/guests/freertos.bin", 0x80000,0x80000,0x80000);
 	
 	while(1){
 		__disable_irq();
