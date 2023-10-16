e5f678ddbeb3782a528e148ae148f77cf0d08d56
yuvraj1803 <ysakshith@gmail.com>
2023-10-16 20:33:09 +0530

    added help() shell command

--- shell/shell.c
+++ shell/shell.c
@@ -5,15 +5,20 @@
 #include "stdio.h"
 #include "string.h"
 #include "core/vm.h"
+#include "drivers/uart.h"
+#include "stdlib.h"
 
 // prototypes for shell commands
 void list();
+void enter();
+void help();
 
 struct shell* delta_shell;
 
 uint8_t SHELL_ACTIVE_BEFORE = 0; // this is set to 0 when we go out of the shell to some other shell.
 extern int total_vms;
 extern struct vm* vmlist[CONFIG_MAX_VMs];
+extern uint8_t vm_connected_to_uart;
 
 struct shell_command{
     char command[CONFIG_MAX_SHELL_COMMAND_SIZE];  
@@ -21,6 +26,9 @@ struct shell_command{
 };
 
 struct shell{
+    uint64_t in;
+    uint64_t out;
+    char buffer[CONFIG_SHELL_BUFFER_SIZE];
     struct shell_command* commands[CONFIG_MAX_SHELL_COMMANDS];
 };
 
@@ -64,12 +72,11 @@ void shell_run(){
 
 void shell_init(){
     struct shell* delta_shell = (struct shell*) malloc(sizeof(struct shell));
-    delta_shell->in = 0;
-    delta_shell->out = 0;
-    memset(delta_shell->buffer, 0, sizeof(delta_shell->buffer));
-    memset(delta_shell->commands, 0, sizeof(delta_shell->commands));
+    memset(delta_shell, 0, sizeof(delta_shell));
 
     add_shell_command("list", list);
+    add_shell_command("enter", enter);
+    add_shell_command("help", help);
 
     log("Shell initialised\n");
 }
@@ -84,4 +91,16 @@ void list(){
         printf("Size: %d\n", vmlist[vm]->vmdata_size);
         printf("\n");
     }
+}
+
+void enter(){
+ 
+}
+
+void help(){
+    printf("\n");
+    printf("list: \tList all VMs and their status.\n");
+    printf("enter: \tEnter a VM.\n");
+    printf("help: \tList all commands and their functions.\n");
+    printf("\n");
 }
