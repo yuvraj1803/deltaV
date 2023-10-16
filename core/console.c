adbc50846c29e8619874de6651a6442d7c5935a4
yuvraj1803 <ysakshith@gmail.com>
2023-10-16 20:50:40 +0530

    added console print and fixed reverse placement of characters in console buffer. ie, begin confused with end.

--- core/console.c
+++ core/console.c
@@ -3,16 +3,16 @@
 void console_push(struct vm_console* console, int val){
     if(console_full(console)) return;
 
-    console->buffer[console->begin] = val;
-    console->begin = (console->begin + 1)%CONFIG_VM_CONSOLE_BUFFER_SIZE;
+    console->buffer[console->end] = val;
+    console->end = (console->end + 1)%CONFIG_VM_CONSOLE_BUFFER_SIZE;
     console->used++;
 }
 
 int console_pop(struct vm_console* console){
     if(console_empty(console)) return -1;
 
-    int out = console->buffer[console->end];
-    console->end = (console->end + 1) % CONFIG_VM_CONSOLE_BUFFER_SIZE;
+    int out = console->buffer[console->begin];
+    console->begin = (console->begin + 1) % CONFIG_VM_CONSOLE_BUFFER_SIZE;
     console->used--;
 
     return out & 0xff;
@@ -30,4 +30,12 @@ void console_clear(struct vm_console* console){
     console->begin = 0;
     console->end = 0;
     console->used = 0;
+}
+
+void console_print(struct vm_console* console){
+    int ptr = console->begin;
+    while(console->buffer[ptr] && ptr != console->end){
+        printf("%c", console->buffer[ptr]);
+        ptr = (ptr + 1)%CONFIG_VM_CONSOLE_BUFFER_SIZE;
+    }
 }
