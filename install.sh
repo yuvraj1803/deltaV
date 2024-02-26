001827b536ae2714352e1311c75203a676956070
yuvraj1803 <ysakshith@gmail.com>
2024-02-26 17:09:15 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt install qemu-system-aarch64 -y
 sudo apt install gcc-aarch64-linux-gnu -y
+sudo apt install gdb-multiarch -y
 mkdir build
 cd build
 mkdir boot core debug drivers fs lib mm shell sse
